29ce255e1d878ac863d01415ca2740bfdc78c87c
Colin Clark <colinbdclark@gmail.com>
2014-05-18 18:44:53 -0400

    Better stability with 8-bit resolution. Scales values to 1.0.

--- flockBox/flockBox.ino
+++ flockBox/flockBox.ino
@@ -3,66 +3,56 @@
 #include <OSCMessage.h>
 #include <SLIPEncodedUSBSerial.h>
 
-const float LOG001 = log(0.001);
+const int DELAY_RATE = 1;
+const int NUM_POTS = 4;
+const int RESOLUTION = 8;
+const float MAX_VAL = (float)pow(2, RESOLUTION) - 1.0;
+const String ADDR_PREFIX = "/knobs/";
 
-const int delayRate = 1;
-const int numPots = 4;
-const int maxUnscaledValue = 4095;
-const String addrPrefix = "/pots/";
-
-int potPins[numPots] = {0, 1, 2, 3};
-int prevVals[numPots] = {0.0, 0.0, 0.0, 0.0};
-
+int potPins[NUM_POTS] = {0, 1, 2, 3};
+byte prevVals[NUM_POTS] = {0, 0, 0, 0};
 SLIPEncodedUSBSerial SLIPSerial(Serial);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200); // Rate is ignored for USB devices such as the Teensy.
+  analogReadResolution(RESOLUTION);
+  analogReadAveraging(32);
+  analogRead(0);
 }
 
 void loop() {
-  for (int i = 0; i < numPots; i++) {
-    analogReadResolution(12);
-    analogReadAveraging(32);
-    
+  for (int i = 0; i < NUM_POTS; i++) {
     int potPin = potPins[i];
-    int prevVal = prevVals[i];
-    int val = analogRead(potPin);
-    double scaled;
-    
+    byte prevVal = prevVals[i];
+    byte val = analogRead(potPin);
+
     if (val != prevVal) {
-      //scaled = scale(val);
-      handleValueOSC(potPin, val);
+      float scaled = (float)val / MAX_VAL;
+      handleValueOSC(potPin, scaled);
     }
-    
-    prevVals[i] = val;    
+    prevVals[i] = val;
+
+    delay(DELAY_RATE);
   }
-  
-  delay(delayRate);
 }
 
-double scale(int val) {
-  return (double) (val / maxUnscaledValue);
-}
-
-void handleValueOSC(int potPin, int val) {
-  String address = String(addrPrefix + potPin);
+void handleValueOSC(int potPin, float val) {
+  String address = String(ADDR_PREFIX + potPin);
   int addressLength = address.length() + 1;
   char addressBuffer[addressLength];
-  
+
   address.toCharArray(addressBuffer, addressLength);
   OSCMessage msg(addressBuffer);
   msg.add(val);
-  
+
   msg.send(SLIPSerial);
   SLIPSerial.endPacket();
   msg.empty();
 }
 
-void handleValueDebug(int potPin, int val) {
-  Serial.print("Pot ");
+void handleValueDebug(int potPin, float val) {
+  Serial.print("Knob ");
   Serial.print(potPin);
   Serial.print(": ");
   Serial.println(val);
 }
-
-
